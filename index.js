dc11aae7a00d2c3bca34159fb7f37db4eeb5927e
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-03-22 16:46:27 +0200

    lesson 35 task 3 v2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-//import { renderer } from './src/scripts/renderer.js';
+import { renderReposList } from './src/scripts/renderReposList.js';
+import { renderUserData } from './src/scripts/renderUserData.js';
 import { showSpinner } from './src/scripts/spinner.js';
 import { getReposList, getUserData } from './src/scripts/workWithServer.js';
 
@@ -11,44 +12,29 @@ import { getReposList, getUserData } from './src/scripts/workWithServer.js';
 const userAvatarElem = document.querySelector('.user__avatar');
 userAvatarElem.src = 'https://avatars3.githubusercontent.com/u10001';
 
-const userNameElem = document.querySelector('.user__name');
-const userLocationElem = document.querySelector('.user__location');
 const nameFormInputElem = document.querySelector('.name-form__input');
-const repoListElem = document.querySelector('.repo-list');
 
 const onShowBtnHandler = () => {
   showSpinner(true);
 
   getUserData(nameFormInputElem.value)
     .then(userData => {
-      const { avatar_url, name, location, repos_url } = userData;
-      userAvatarElem.src = avatar_url;
-      userNameElem.textContent = name;
-      userLocationElem.textContent = `from ${location}`;
-
-      return repos_url;
+      renderUserData(userData);
+      console.log(userData.repos_url);
+      return userData.repos_url;
     })
     .then(url => getReposList(url))
     .then(reposlist => {
-      const reposElems = [...reposlist].map(({ name }) => {
-        const repoElem = document.createElement('li');
-        repoElem.classList.add('repo-list__item');
-        repoElem.textContent = name;
-        return repoElem;
-      });
-      repoListElem.append(...reposElems);
+      renderReposList(reposlist);
     })
     .then(() => {
-      showSpinner(false);
       nameFormInputElem.value = '';
     })
-    .catch(() => {
-      alert('Failed to load data');
+    .catch(err => alert(err.message))
+    .finally(() => {
       showSpinner(false);
     });
 };
 
 const showBtn = document.querySelector('.name-form__btn');
 showBtn.addEventListener('click', () => onShowBtnHandler());
-//const showBtn = document.querySelector('.name-form__btn');
-//showBtn.addEventListener('click', () => renderer());
--- src/scripts/workWithServer.js
+++ src/scripts/workWithServer.js
@@ -1,9 +1,19 @@
 const mainUrl = 'https://api.github.com/users';
 
 export const getUserData = userName => {
-  return fetch(`${mainUrl}/${userName}`).then(userData => userData.json());
+  return fetch(`${mainUrl}/${userName}`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Failed to load data');
+  });
 };
 
 export const getReposList = url => {
-  return fetch(url).then(reposList => reposList.json());
+  return fetch(url).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Failed to load data');
+  });
 };
